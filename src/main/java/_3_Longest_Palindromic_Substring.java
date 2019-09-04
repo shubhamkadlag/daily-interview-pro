b54f48488dc11a031fd7ff3af248fe7322f7ca47
Shubham Kadlag <shubhaka@amdocs.com>
2019-09-04 10:57:14 +0530

    Daily Interview Pro #3

---
+++ src/main/java/_3_Longest_Palindromic_Substring.java
@@ -0,0 +1,46 @@
+package main.java;
+
+public class _3_Longest_Palindromic_Substring {
+
+    public static void main(String[] args) {
+        System.out.println(longestPalindrome("cbbd"));// ans -> bb
+        System.out.println(longestPalindrome("abababa"));// ans -> abababa
+        System.out.println(longestPalindrome("abcdefg"));// ans -> a
+        System.out.println(longestPalindrome(""));// ans -> ""
+    }
+    public static String longestPalindrome(String s) {
+        if(s == null || s.length() < 1)
+            return "";
+        int maxLen = 1;
+        int start = 0;
+        for(int i = 1 ; i < s.length() ; i++) {
+            int currLen = 1;
+            int l = i - 1;
+            int r = i + 1;
+            while(l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+                currLen += 2;
+                if(currLen > maxLen) {
+                    maxLen = currLen;
+                    start = l;
+                }
+                l--;
+                r++;                
+            }            
+            l = i - 1;
+            r = i;
+            currLen = 0;            
+            
+            while(l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+                currLen += 2;
+                if(currLen > maxLen) {
+                    maxLen = currLen;
+                    start = l;
+                }
+                l--;
+                r++;
+            }
+        }
+        
+        return s.substring(start, start + maxLen);        
+    }
+}
